81074e30aa72f231e55cb13d149b26ac7ff29fdd
callidaria <lo.true@gmx.de>
2021-11-26 00:49:59 +0100

    more annotation features for cmli

--- script/menu_list.cpp
+++ script/menu_list.cpp
@@ -7,7 +7,12 @@
 MenuList::MenuList()
 {
 	Font fproc = Font("res/fonts/nimbus_roman.fnt","res/fonts/nimbus_roman.png",0,0); // TODO: minimize
-	dtxt.push_back(Text(fproc));
+	//dtxt.push_back(Text(fproc)); // FIXME: breakdown if necessary in new update
+	LEntity proc = {
+		Text(fproc),Text(fproc),std::vector<std::string>(),std::vector<std::string>(),
+		false,0,0,0,0,0,""
+	};
+	les.push_back(proc);
 	esize = 0;
 }
 
@@ -16,13 +21,18 @@ MenuList::MenuList()
 	cam2d: text will be rendered according to the camera parameter's perspective and view
 	path: path to menu list configuration file in updated cmli2 language format
 	purpose: constructor, that interprets single cmli2 file and sets up the menu list accordingly
+	cmli syntax definitions:
+		<head>*content*</head>
+		<desc>*description*</desc>
+		(optional)<le>*element*,*value*</le>
+		(optional)<slider>*slider min (int)*,*slider max (int)*</slider>
+		(optional)<dest>*destination save name in config*</dest>
 */
 MenuList::MenuList(Camera2D* cam2d,const char* path)
 {
 	// setting up the different fonts & texts for menu parts
 	Font lfnt = Font("res/fonts/nimbus_roman.fnt","res/fonts/nimbus_roman.png",30,30);
 	Font dfnt = Font("res/fonts/nimbus_roman.fnt","res/fonts/nimbus_roman.png",15,15);
-	ltxt = Text(lfnt); // variable, storing the main list entity text
 	Text t_desc;	   // temporary storage variable for node descriptions
 	// FIXME: do not make the fonts with static parameters
 
@@ -32,6 +42,8 @@ MenuList::MenuList(Camera2D* cam2d,const char* path)
 	std::string t_content;			 // temporary node content
 	std::string t_line = "";		 // temporary line content for description text before break
 	bool ractive = false;			 // active reading process (in node)
+
+	// extracting raw node data from file
 	while (!mlfile.eof()) {
 		std::string line;getline(mlfile,line);	  // reading line by line
 		bool enode = !strcmp(line.c_str(),"</>"); // end node boolean true if line contains end annotation
@@ -43,14 +55,21 @@ MenuList::MenuList(Camera2D* cam2d,const char* path)
 			ndata.push_back(t_content); // save aquired content inside data list
 			t_content = "";		    // reset content variable
 		} else if (!strcmp(line.c_str(),"<node>")) ractive = true; // open node if marked accordingly
-	} for (int i=0;i<ndata.size();i++) {
-		uint32_t ix = 0; // index of raw node data
+	}
+
+	// interpreting raw node data as cmli pattern
+	std::vector<std::string> args;
+	std::string t_extract;
+	for (int i=0;i<ndata.size();i++) {
+		uint32_t ix = 0;     // index of raw node data
+		struct LEntity t_le; // temporary list element, getting pushed back later
+		t_le.ltxt = Text(lfnt);
 		while (ix<ndata[i].size()) {
 			uint8_t emode = get_readmode(ndata[i],ix);   // check character for mode annotation
 			std::string excnt = breakgrind(ndata[i],ix); // get content inside annotation area
 			switch (emode) {
 			case 1: // conservative menu list element
-				ltxt.add(excnt.c_str(),glm::vec2(250,lscroll)); // write to main list elements
+				t_le.ltxt.add(excnt.c_str(),glm::vec2(250,lscroll)); // write to main list element
 				lscroll -= 45; // scroll down after write
 				esize++;       // inc list element counter
 				break;
@@ -64,29 +83,45 @@ MenuList::MenuList(Camera2D* cam2d,const char* path)
 					} else t_line += excnt[iy]; // add innocent character to temporary line
 				} t_desc.add(t_line.c_str(),glm::vec2(920,dscroll)); // add leftover line to desc
 				t_desc.load_wcam(cam2d);			     // load written desc data
-				dtxt.push_back(t_desc); // save description in the description list
-				dscroll = 600;		// reset cursor scroll
-				t_line = "";		// reset temporary line
+				t_le.dtxt = t_desc; // save description in the description list
+				dscroll = 600;	    // reset cursor scroll
+				t_line = "";	    // reset temporary line
 				break;
-			default:printf("%s is a broken cmli file\n",path);
+			case 3: // selectable menu sublist element in dropdown
+				// TODO: split integer interpretation according to set pattern
+				// TODO: read from system for some list elements (eg. monitor ID)
+				args = split_arguments(excnt); // getting argument list
+				t_le.lee.push_back(args[0]); // save entity parameter as sublist element
+				t_le.lev.push_back(args[1]); // save value parameter as value list element
+				break;
+			case 4: // slider element inserted
+				// TODO: interpret slider parameters as integer construct
+				t_le.slide = true; // setting slider to be active and visible
+				args = split_arguments(excnt); // define the capacities according to pattern
+				t_le.sl_min = stoi(args[0]);
+				t_le.sl_max = stoi(args[1]);
+				t_le.sl_vmin = stoi(args[2]);
+				t_le.sl_vmax = stoi(args[3]);
+				break;
+			case 5: // destination annotation
+				t_le.saveID = excnt;
+				break;
+			default:printf("%s uses unknown or outdated annotation ID: %i\n",path,emode);
 			}
-		} ltxt.load_wcam(cam2d); // load conservative list element data
+		} t_le.ltxt.load_wcam(cam2d); // load conservative list element data
+		les.push_back(t_le);
 	} // FIXME: text with annotated \n in written file format -> is it \n or \\n?
-}
+} 	// TODO: write interpreter fault instead of letting the mem take the fall
+	// TODO: read selection status from config file
 
 /*
-	add_lines(Camera2D*,std::vector<const char*>)
-	cam2d: text will be rendered according to the camera parameter's perspective and view
-	lines: list of lines that will be added to the menu list
-	purpose: good question.
-	TODO: PREPARE FOR REMOVAL
+	save(void)
+	purpose: identify destination & write all changes to config file
 */
-void MenuList::add_lines(Camera2D* cam2d,std::vector<const char*> lines)
+void MenuList::save()
 {
-	for (int i=0;i<lines.size();i++) {
-		ltxt.add(lines.at(i),glm::vec2(250,lscroll));
-		lscroll-=45;esize++;
-	} ltxt.load_wcam(cam2d);
+	// TODO: destination check
+	// TODO: writing routine
 }
 
 /*
@@ -98,20 +133,26 @@ void MenuList::add_lines(Camera2D* cam2d,std::vector<const char*> lines)
 */
 void MenuList::render(float dtrans,float lscroll,uint16_t index)
 {
-	// rendering the selectable list text
-	ltxt.prepare();
-	int32_t fscroll = lscroll*45; // calculating the amount of scrolling
-	ltxt.set_scroll(glm::translate(glm::mat4(1.0f),glm::vec3(rand()%10,fscroll+rand()%10,0))); // shadow scrll
-	ltxt.render(dtrans*2048,glm::vec4(.35f,.35f,.35f,.5f)); 		 // rendering fake shadow
-	ltxt.set_scroll(glm::translate(glm::mat4(1.0f),glm::vec3(0,fscroll,0))); // list scroll in shader
-	ltxt.render(dtrans*2048,glm::vec4(1,1,1,1)); // rendering the main text
-	// ??maybe do shadow calculation in shader
-	// FIXME: reduce call to just translation from mat4(1) and translation from model
-	// FIXME: performance
+	for (int i=0;i<les.size();i++) {
+		// rendering the selectable list text
+		les.at(i).ltxt.prepare();
+		int32_t fscroll = lscroll*45; // calculating the amount of scrolling
+		les.at(i).ltxt.set_scroll(glm::translate(glm::mat4(1.0f),
+			glm::vec3(rand()%10,fscroll+rand()%10,0)));			 // shadow scroll
+		les.at(i).ltxt.render(dtrans*2048,glm::vec4(.35f,.35f,.35f,.5f)); 	 // rendering fake shadow
+		les.at(i).ltxt.set_scroll(glm::translate(glm::mat4(1.0f),
+			glm::vec3(0,fscroll,0)));					 // list scroll in shader
+		les.at(i).ltxt.render(dtrans*2048,glm::vec4(1,1,1,1));			 // render main text
+		// ??maybe do shadow calculation in shader
+		// FIXME: reduce call to just translation from mat4(1) and translation from model
+		// FIXME: reduce max render cap after broken down save system
+		// FIXME: performance
 
-	// rendering the description text in white
-	dtxt.at(index).prepare();
-	dtxt.at(index).render(dtrans*1024,glm::vec4(1,1,1,1));
+		// rendering the description text in white
+		les.at(i).dtxt.prepare();
+		les.at(i).dtxt.render(dtrans*1024,glm::vec4(1,1,1,1));
+		// FIXME: broken render flow
+	}
 }
 
 /*
@@ -141,7 +182,7 @@ std::string MenuList::breakgrind(std::string nl,uint32_t &i)
 		0 := faulty bracket contents
 		1 := linebreak annotation with "<br>"
 */
-uint8_t MenuList::textgrind(std::string fline,uint32_t &i)
+uint8_t MenuList::textgrind(std::string fline,uint32_t &i) // FIXME: collapsable into grind or getter??
 {
 	if (fline[i]=='<') { // character is not innocent
 		std::string cast = "";	  // variable for readable cast
@@ -163,6 +204,9 @@ uint8_t MenuList::textgrind(std::string fline,uint32_t &i)
 		0 := faulty element identifier read "<*>"
 		1 := conservative menu list element marked as "<head>"
 		2 := menu splash description text element marked as "<dsc>"
+		3 := dropdown list element marked as "<le>"
+		4 := slider element marked as "<slider>"
+		5 := save destination annotation marked as "<dest>"
 */
 uint8_t MenuList::get_readmode(std::string nl,uint32_t &i)
 {
@@ -173,5 +217,23 @@ uint8_t MenuList::get_readmode(std::string nl,uint32_t &i)
 		pcnt += nl[i];	// add current character to the cast variable
 		i++;		// increment line index
 	} i++;			// ready for further work with the nodeline
-	return !strcmp(pcnt.c_str(),"head")+2*!strcmp(pcnt.c_str(),"dsc"); // output ID according to annotation
+	return !strcmp(pcnt.c_str(),"head")+2*!strcmp(pcnt.c_str(),"dsc")+3*!strcmp(pcnt.c_str(),"le")
+		+4*!strcmp(pcnt.c_str(),"slider")+5*!strcmp(pcnt.c_str(),"dest");
+	// output ID according to annotation
 }
+
+/*
+	split_arguments(std::string)
+	ext: represents the extraction string
+	purpose: getting an argument list from extraction string
+*/
+std::vector<std::string> MenuList::split_arguments(std::string ext)
+{
+	std::vector<std::string> args;
+	std::string t_arg;
+	for (int i=0;i<ext.size();i++) {
+		if (ext[i]==',') args.push_back(t_arg);
+		else t_arg += ext[i];
+	} args.push_back(t_arg);
+	return args;
+} // TODO: optimize
--- script/menu_list.h
+++ script/menu_list.h
@@ -8,22 +8,37 @@
 #include "../ccb/fcn/font.h"
 #include "../ccb/fcn/text.h"
 
+
+//	To save list entity in a less cluttered way
+struct LEntity
+{	
+	Text ltxt,dtxt;			// variables for list entity text & description
+	std::vector<std::string> lee;	// list of list entity elements in sublist
+	std::vector<std::string> lev;	// list of list entity flag values to write to config
+	bool slide = false;		// identifier of slideable values in list entity
+	uint32_t sl_min,sl_max;		// definition of visibile slider caps
+	float sl_vmin,sl_vmax;		// definition of slider save caps in file
+	uint32_t sID = 0;		// selection ID for active chosen option / slider input
+	std::string saveID;		// value, storing the config variable destination
+	// TODO: elements for save destination, values & additional parsing
+};
+
 class MenuList
 {
 public:
 	MenuList();
 	MenuList(Camera2D* cam2d,const char* path);
 	~MenuList() {  }
-	void add_lines(Camera2D* cam2d,std::vector<const char*> lines);
+	void save();
 	void render(float dtrans,float lscroll,uint16_t index);
 private:
 	std::string breakgrind(std::string nl,uint32_t &i);
 	uint8_t textgrind(std::string nl,uint32_t &i);
 	uint8_t get_readmode(std::string nl,uint32_t &i);
+	std::vector<std::string> split_arguments(std::string ext);
 public:
 	uint16_t esize = 0;
 private:
-	Text ltxt;
-	std::vector<Text> dtxt; // ??maybe do this with less SPACE
+	std::vector<struct LEntity> les;
 	int32_t lscroll = 515,dscroll = 600;
 };
