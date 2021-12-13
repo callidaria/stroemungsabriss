d269a08b0974f8e87edc8f4584421782f95e071c
callidaria <lo.true@gmx.de>
2021-12-13 20:33:16 +0100

    fixed advanced config based init

--- ccb/fcn/init.h
+++ ccb/fcn/init.h
@@ -15,6 +15,7 @@ public:
 	uint32_t rINT(uint32_t id);
 private:
 	uint32_t find_iKey(const char* key);
+	std::vector<uint32_t> read_cartesian(std::string val);
 public:
 	// keys
 	const uint32_t FRAME_RESOLUTION_WIDTH = 0;
@@ -23,8 +24,8 @@ public:
 	const uint32_t FRAME_DISPLAY_ID = 3;
 private:
 	// key identification dictionaries
-	std::vector<const char*> iKeys = { "frameResWidth","frameResHeight","frameFullScreen",
-		"frameDisplayID" };
+	std::vector<const char*> iKeys = { "vFrameResolution","vFrameResolution","bFrameFullscreen",
+		"iFrameTargetMonitor" };
 
 	// constant collections
 	uint32_t iConfig[INTEGER_KEY_LENGTH];
--- ccb/src/init.cpp
+++ ccb/src/init.cpp
@@ -4,13 +4,18 @@ Init::Init(const char* path)
 {
 	std::ifstream file(path,std::ios::in);
 	while (!file.eof()) {
-		char* lh = new char[128];file >> lh;
-		const char* rclh = reinterpret_cast<char*>(&lh[0]);
-		char* val = new char[64];file >> val;
-		iConfig[find_iKey(rclh)] = atoi(val);
+		std::string rclh;file >> rclh;
+		std::string val;file >> val;
+		std::vector<uint32_t> ex = read_cartesian(val);
+		uint16_t j = find_iKey(rclh.c_str());
+		for (int i=0;i<ex.size();i++) {
+			iConfig[j+i] = ex[i];
+		}
 	}
-}
+} // FIXME: find a faster way to do this
+
 uint32_t Init::rINT(uint32_t id) { return iConfig[id]; }
+
 uint32_t Init::find_iKey(const char* key)
 {
 	int out = 0;
@@ -19,3 +24,23 @@ uint32_t Init::find_iKey(const char* key)
 		out++;
 	} return out;
 }
+
+std::vector<uint32_t> Init::read_cartesian(std::string val)
+{
+	std::string proc;
+	std::vector<std::string> raw_out;
+	for (int i=0;i<val.size();i++) {
+		if (val[i]=='x') {
+			raw_out.push_back(proc);
+			proc = "";
+		} else proc += val[i];
+	} raw_out.push_back(proc);
+	std::vector<uint32_t> out;
+	for (int i=0;i<raw_out.size();i++) {
+		if (raw_out[i].size()>0) out.push_back(std::stoi(raw_out[i]));
+		// FIXME: delete check and find out why it was necessary
+	}
+	return out;
+}
+
+// FIXME: OUTDATED WRITING & CONCEPTS! UPDATE THIS IN THE FUTURE
