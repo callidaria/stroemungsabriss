a1a24fed2786fef2afd4d2b02ceeca89183a42fb
callidaria <lo.true@gmx.de>
2022-09-13 01:00:30 +0200

    possibility to open packages

--- README.md
+++ README.md
@@ -84,5 +84,5 @@ double click yomisensei.exe
     + 54: 12h 40min
     + 20: 3h 8min
     + 67: 1h 27min
-    + 22: 2h 42min
-- Total Code Lines: 6564
+    + 22: 3h 13min
+- Total Code Lines: 7062
--- ccb/ccb.cpp
+++ ccb/ccb.cpp
@@ -8,11 +8,15 @@
 #define RESET "\033[0m"
 #define SELECT "\033[1;32m"
 
+#define ENGINE_ROOT "./ccb"
+#define PROJECT_ROOT "./script"
+
 // toolbox
 void write_selection();
 bool get_selected();
 
 // engine features
+void offer_root(std::string &dir_path,std::string rt_dir);
 std::string read_components(std::string &dir_path);
 std::string build_engine_component();
 std::string build_project_component();
@@ -21,13 +25,14 @@ std::string count_lines();
 
 // components
 char inp;
+bool update = false;
 uint8_t itr,idx = 0;
 
 int main(int argc,char* argv[])
 {
-	// setup and welcome message
-	printf("welcome to cascabel console\n");
-	std::string out,edir = "./ccb",pdir = "./script";
+	// setup
+	std::string out,edir = ENGINE_ROOT,pdir = PROJECT_ROOT;
+	uint8_t proj_idx;
 
 	// main loop
 	while (true) {
@@ -35,14 +40,20 @@ int main(int argc,char* argv[])
 		// setup component iterator
 		itr = 0;
 
+		// prepare write
+		printf("\033[0m\033[2J\033[1;1HCASCABEL CONSOLE\n");
+
 		// write engine contents
 		printf("\n\n\n\t\t\tENGINE\n");
+		offer_root(edir,ENGINE_ROOT);
 		out += read_components(edir);
 		printf("\n");
 		out += build_engine_component();
+		proj_idx = itr;
 
 		// write project contents
 		printf("\n\n\n\t\t\t%sPROJECT\n",RESET);
+		offer_root(pdir,PROJECT_ROOT);
 		out += read_components(pdir);
 		printf("\n");
 		out += build_project_component();
@@ -53,22 +64,24 @@ int main(int argc,char* argv[])
 		out += count_lines();
 
 		// write output message
-		printf("\033[0m\n\n\n\n> %s\n",out.c_str());
-		printf("\033[30;47m[c] jump to engine    [p] jump to project    [e] exit\033[0m\n");
+		printf("\033[0m\n\n\n\n> %s\033[30;47m\n",out.c_str());
+		printf("[r] run game  [b] build main  [SPACE] run selected  [o] open  [c] jump to engine  [p] jump to project  [e] exit\033[0m\n");
 
 		// read user input
-		system("stty raw");
-		inp = getchar();
-		system("stty cooked");
+		if (!update) {
+			system("stty raw");
+			inp = getchar();
+			system("stty cooked");
+		} else update = false;
 
 		// kill when exit request
-		if (inp=='e') break;
+		if (inp=='e') { system("clear");break; }
+		else if (inp=='r') system("./yomisensei");
+		else if (inp=='c') idx = 0;
+		else if (inp=='p') idx = proj_idx;
 
 		// move selector
 		idx += (inp=='B'&&idx<itr-1)-(inp=='A'&&idx>0);
-
-		/*else if (command=="build") system("ccb/ccb_build.sh");
-		else printf("command not valid\n");*/
 	}
 	return 0;
 }
@@ -84,6 +97,25 @@ bool get_selected()
 	return itr==idx&&inp==' ';
 }
 
+void offer_root(std::string &dir_path,std::string rt_dir)
+{
+	if (dir_path!=rt_dir) {
+
+		// render offer
+		write_selection();
+		printf(" ROOT\n");
+
+		// check root request
+		if (get_selected()) {
+			dir_path = rt_dir;
+			update = true;
+		}
+
+		// prepare next
+		itr++;
+	}
+}
+
 std::string read_components(std::string &dir_path)
 {
 	//open directory
@@ -96,6 +128,13 @@ std::string read_components(std::string &dir_path)
 
 		// print current component if not parent or this
 		if (found->d_name[0]!='.') {
+
+			// open directory on demand
+			if (itr==idx&&inp=='o'&&found->d_type==DT_DIR) {
+				dir_path += "/"+std::string(found->d_name);
+				idx = 0;
+				update = true;
+			}
 
 			// print selection
 			write_selection();
@@ -158,7 +197,8 @@ std::string count_lines()
 	printf(" COUNT LINES\n");
 
 	// run in chosen
-	if (get_selected()) system("find ccb/src/ ccb/aud/ ccb/fcn/ ccb/gfx/ ccb/net/ ccb/frm/ ccb/mat/ ccb/ppe/ script/ shader/ main.cpp -type f | xargs wc -l | tail -n 1");
+	if (get_selected())
+		system("find ccb/src/ ccb/aud/ ccb/fcn/ ccb/gfx/ ccb/net/ ccb/frm/ ccb/mat/ ccb/ppe/ script/ script/ui/ shader/ main.cpp -type f | xargs wc -l | tail -n 1");
 
 	// prepare next
 	itr++;
